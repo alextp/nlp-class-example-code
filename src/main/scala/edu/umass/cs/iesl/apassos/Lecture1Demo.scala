bc130067dba6794e7bb0815f27f7957c4269c739
Alexandre Passos <alexandre.tp@gmail.com>
2013-09-10 10:07:13 -0400

    Initial commit

---
+++ src/main/scala/edu/umass/cs/iesl/apassos/Lecture1Demo.scala
@@ -0,0 +1,13 @@
+package edu.umass.cs.iesl.apassos
+
+import cc.factorie._
+
+object Lecture1Demo {
+  def main(args: Array[String]) {
+    val doc = new app.nlp.Document("All work and no play makes Jack a dull boy. All work and no play makes Jack a dull boy.")
+    val pipeline = app.nlp.DocumentAnnotatorPipeline[app.nlp.ner.NerLabel,app.nlp.parse.ParseTree]
+    pipeline.process(doc)
+    val printers = for (ann <- Seq(app.nlp.pos.POS1, app.nlp.ner.NER1, app.nlp.parse.DepParser1)) yield (t: app.nlp.Token) => ann.tokenAnnotationString(t)
+    println(doc.owplString(printers))
+  }
+}
